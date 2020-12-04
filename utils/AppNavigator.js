fdff42ff9ab7009eccb01c787ccd97e88c3203cc
sakshiigoyal <sakshigoyal7728@gmail.com>
2020-12-04 22:04:36 +0530

    code refactored

--- utils/AppNavigator.js
+++ utils/AppNavigator.js
@@ -1,24 +1,17 @@
-import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import MainScreenContainer from '../src/MainScreenContainer';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS
-                }}
-                initialRouteName="MainScreenContainer">
-                <Stack.Screen
-                    name='Image List'
-                    component={MainScreenContainer}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MainScreenContainer">
+        <Stack.Screen name="Image List" component={MainScreenContainer} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 export default AppNavigator;
--- utils/Config.js
+++ utils/Config.js
@@ -1,16 +1,10 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import Reducers from './RootReducer';
 import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-
-if (__DEV__) {
-    reducCompose = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
-}
-
 const configureStore = () => {
-    return createStore(Reducers, applyMiddleware(logger, thunk))
-}
+  return createStore(Reducers, applyMiddleware(logger, thunk));
+};
 
 export default configureStore;
