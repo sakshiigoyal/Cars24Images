c52ff2e677564e62c4525b6fdaef7772d92e30c9
sakshigoyalsg <sakshi.goyal@studiographene.com>
2020-12-04 12:28:46 +0530

    working on animation

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,5 +1,5 @@
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import axios from 'axios';
 
 import React from 'react';
@@ -11,9 +11,9 @@ import {
   Text,
   Alert,
   SafeAreaView,
-  ImageBackground,
+  Animated,
   Image,
-  Platform,
+  Easing,
   Linking,
   ScrollView,
   FlatList,
@@ -26,9 +26,23 @@ class MainScreen extends React.Component {
       imageList: [],
       listGrid: true,
     };
+    this.animatedValue = new Animated.Value(0)
+
   }
+
   componentDidMount() {
     this.props.fetchImageList();
+    // Animated.timing(this.animatedValue, {
+    //   toValue: 1,
+    //   duration: 1300,
+    //   useNativeDriver: true,
+    // }).start()
+    Animated.timing(this.animatedValue, {
+      toValue: 1,
+      duration: 1000,
+      easing: Easing.ease,
+      useNativeDriver: true
+    }).start();
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -39,15 +53,21 @@ class MainScreen extends React.Component {
       this.setState({
         imageList: this.props.imageListReducer,
       });
+      console.log('imageListReducer:', this.props.imageListReducer);
     }
   }
 
   render() {
+    // let translateX = this.entranceAnimation.interpolate({
+    //   inputRange: [0, 1],
+    //   outputRange: [-500, 1] // the card will translate from the left side of the screen to its natural position
+    // });
+
     return (
       <SafeAreaView>
         <Switch
-          trackColor={{true: 'black'}}
-          style={{transform: [{scaleX: 0.8}, {scaleY: 0.8}]}}
+          trackColor={{ true: 'black' }}
+          style={{ transform: [{ scaleX: 0.8 }, { scaleY: 0.8 }] }}
           onValueChange={(itemValue, itemIndex) => {
             console.log('itemValue: ', itemValue);
             this.setState({
@@ -57,14 +77,49 @@ class MainScreen extends React.Component {
           value={this.state.listGrid}
         />
 
-        <FlatList
+        <Animated.Image
+          // style={{ transform: [{ translateX }] }}
+          style={{
+            transform: [
+              {
+                translateX: this.animatedValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 120]
+                })
+              },
+              {
+                translateY: this.animatedValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 25]
+                })
+              },
+              {
+                scaleX: this.animatedValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [1, 15]
+                })
+              },
+              {
+                scaleY: this.animatedValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [1, 12.5]
+                })
+              }
+            ],
+            // transform: [{ translateX }],
+            width: 40, height: 40
+          }}
+          source={{ uri: 'https://via.placeholder.com/600/92c952' }}
+        />
+
+        {/* <FlatList
           key={this.state.listGrid ? 'FourColumns' : 'OneColumn'}
           numColumns={this.state.listGrid ? 4 : 1}
           style={styles.listContainer}
           extraData={this.state}
           data={this.state.imageList}
-          renderItem={({item, index}) => (
-            <View key={item.id} style={{flex: 1, marginTop: 8}}>
+          renderItem={({ item, index }) => (
+            <View key={item.id} style={{ flex: 1, marginTop: 8 }}>
               <Text numberOfLines={1} style={styles.titleStyle}>
                 {item.title}
               </Text>
@@ -78,7 +133,7 @@ class MainScreen extends React.Component {
             </View>
           )}
           keyExtractor={(item) => item.postUUID}
-        />
+        /> */}
       </SafeAreaView>
     );
   }
